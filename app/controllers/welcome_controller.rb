2c1c64363ea029b9fc27f3713ed7027df74643a3
Ronju <platinumstar@gmail.com>
2016-08-06 05:45:55 +0000

    built out additional pages

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def pricing
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def features
+  end
+end
